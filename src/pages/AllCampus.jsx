c4205f47eadb70b77b86b13d5d2262bdf5556d33
AnyiC <anyichen212@gmail.com>
2023-06-29 22:25:51 -0400

    add loading to campus page and add msg to notify campus list is empty

--- src/pages/AllCampus.jsx
+++ src/pages/AllCampus.jsx
@@ -1,21 +1,43 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchAllCampusThunk } from '../redux/campuses/campusesActions';
+
 import CampusCard from '../components/CampusCard';
 
 function AllCampus() {
   const allCampus = useSelector((state) => state.campus.allCampus);
   const dispatch = useDispatch();
-  const fetchAllCampus = () => {
-    console.log("Distpatch from FetchAllCampus");
-    return dispatch(fetchAllCampusThunk());
-  };
+
 
   useEffect(() => {
+    const fetchAllCampus = () => {
+      console.log("Distpatch from FetchAllCampus");
+      return dispatch(fetchAllCampusThunk());
+    };
     console.log("useEffect : fetch all campus");
     fetchAllCampus();
     console.log("All Campus : ", allCampus);
-  });
+  }, []);
+
+  //loading if allCampus list is null
+  if(!allCampus){
+    return (
+      <h2>LOADING...</h2>
+    )
+  }
+  
+  //if no campus > campus length is 0
+  if(allCampus.length === 0){
+    return (
+      <div>
+        <h1>AllCampus</h1>
+        <div>No Campus here. Be the first to add one!</div>
+        <div><button>All Campus</button></div>
+      </div>
+
+    )
+  }
+
 
   return (
     <div>
@@ -23,7 +45,7 @@ function AllCampus() {
       <div>
         <div><button>All Campus</button></div>
         {allCampus.map((item) => {
-          return <CampusCard campus={item} />
+          return <CampusCard key={item.id} campus={item} />
         })}
       </div>
     </div>
