16e143edcb8b1f2018c4f4033013a9c827bcd9ff
AnyiC <anyichen212@gmail.com>
2023-07-06 13:31:41 -0400

    style homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BsPostcardHeartFill } from 'react-icons/bs'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div style={{textAlign:'center'}}>
+      <h1>WELCOME</h1>
+      <img style={{width:'60%'}} src="https://1.bp.blogspot.com/-XKTafTy2_Bw/UOKDYzJbICI/AAAAAAAAKNY/tTeng5oVcJk/s1600/school.png" alt='Campus' />
+      <h4 style={{fontSize:'50px'}}>TTP CRUD APP</h4>
+      <h4><BsPostcardHeartFill /> <br /> Anyi Chen</h4>
+    </div>
   )
 }
 
